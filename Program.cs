b3d774aa0af00b41c809424f228b87c33ca557d6
Kevin Erath <KevinErath@users.noreply.github.com>
2015-06-27 23:17:56 +0200

    First checkin

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var tree = CSharpSyntaxTree.ParseText(
+            @"using System; 
+            class Program
+            { 
+                static void Main() 
+                { 
+                    Console.WriteLine(""Hallo, Welt!""); 
+                } 
+            }");
+        var root = tree.GetRoot();
+        foreach (var node in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+        {
+            Console.WriteLine(node.Identifier);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ListClasses
+
+Small sample application to demonstrate usage of Microsoft Roslyn.
+
+More details can be found here:
+http://www.code-whisperer.de/2015/05/03/rosalinde
